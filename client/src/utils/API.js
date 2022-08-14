6ff8d31c083e555fddd7b3cabf97ba7c9ae1900e
dennismacc <dennis.maccarthy@gmail.com>
2022-08-13 20:32:34 -0700

    add starter code for login, signup, api and auth. need to update routes in api

---
+++ client/src/utils/API.js
@@ -0,0 +1,59 @@
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+    return fetch('/api/users/me', {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  
+  export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+
+  // save book data for a logged in user
+  // export const saveBook = (bookData, token) => {
+      //     return fetch('/api/users', {
+          //       method: 'PUT',
+          //       headers: {
+              //         'Content-Type': 'application/json',
+              //         authorization: `Bearer ${token}`,
+              //       },
+              //       body: JSON.stringify(bookData),
+              //     });
+              //   };
+
+ // TODO: SAVE habits and todos for a logged in user
+
+
+// remove saved book data for a logged in user
+//   export const deleteBook = (bookId, token) => {
+//     return fetch(`/api/users/books/${bookId}`, {
+//       method: 'DELETE',
+//       headers: {
+//         authorization: `Bearer ${token}`,
+//       },
+//     });
+//   };
+
+// TODO: DELETE saved habits and todos for a logged in user 
+  
+
+  
