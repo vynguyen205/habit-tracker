2ce426496da159d56565644b34c7eecc40abe46d
Vy Nguyen <vynguyen0520@gmail.com>
2022-08-13 21:32:56 -0700

    added routes --- not yet finished

---
+++ server/routes/api/tagRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+// Get all tags
+
+
+// Get one tag
+
+
+// Create a tag
+
+
+// Update a tag
+
+
+// Delete a tag
+
+
+
+module.exports = router;
---
+++ server/routes/api/todoRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Todo = require('../../models/Todo');
+
+// Get all todos
+
+// Get one todo
+
+// Create a todo
+
+// Update a todo
+
+// Delete a todo
+
+module.exports = router;
