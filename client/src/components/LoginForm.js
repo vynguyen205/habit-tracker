6534cf6695b081514a5cdac268381934ee7a89ce
Marina H <mxhuisken@gmail.com>
2022-08-15 21:14:11 -0700

    pages and components

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert } from 'tailwindcss';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
