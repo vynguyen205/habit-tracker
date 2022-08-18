7728ee4fae7ad3e1291540cf86f8f4326d657d2c
dennismacc <dennis.maccarthy@gmail.com>
2022-08-18 01:27:08 -0700

    created modal for habits. need to add new habit to db and display to page

--- client/src/components/Habits.js
+++ client/src/components/Habits.js
@@ -1,6 +1,7 @@
+// TODO: save new habit to database
+// TODO: add new habit to list of habits that are displayed
+
 import React, { useState } from "react";
-import { Button } from "reactstrap-bootstrap/Button";
-import { Modal } from "reactstrap-bootstrap/Modal";
 import "../App.css";
 
 // Get all habits for a logged in user
@@ -16,15 +17,51 @@ function Habits() {
         getHabits();
     } , []);
 
-    // Add new habit
-    const [show, setShow] = useState(false);
+    // Add new habit to databasea and display it on the page
+        const [showModal, setShowModal] = useState(false); 
+        const [habit, setHabit] = useState({});
+        
+        // TODO: additional functionality for habits page 
+        // const handleSubmit = async (e) => {     
+        //     e.preventDefault();
+        //     const response = await fetch('../utils/api', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({
+        //             name: e.target.name.value,
+        //             description: e.target.description.value,
+        //             frequency: e.target.frequency.value,
+        //             tags: e.target.tags.value
+        //         })
+        //     });
+        //     const data = await response.json();
+        //     setHabits([...habits, data]);
+        //     setShowModal(false);
+        // }
+        // const handleClose = () => {
+        //     setShowModal(false);
+        // }
+        // const handleShow = () => {
+        //     setShowModal(true);
+        // }
+        // const handleDelete = async (id) => {
+        //     const response = await fetch(`../utils/api/${id}`, {
+        //         method: 'DELETE'
+        //     });
+        //     const data = await response.json();
+        //     setHabits(habits.filter(habit => habit.id !== id));
+        // }
+        // const handleEdit = async (id) => {
+        //     const response = await fetch(`../utils/api/${id}`);
+        //     const data = await response.json();
+        //     setHabits(habits.map(habit => habit.id === id ? data : habit));
+        // }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
 
-    return (
-        <>
+        return (
+          <>
         <div className="flex-column justify-left align-left min-100-vh bg-slate-500">
             <ul>
                 {habits.map(habit => (
@@ -34,9 +71,66 @@ function Habits() {
                 ))}
             </ul>
         </div>
-        </>
-    );
-}
-
-
+            <button
+              className="bg-blue-200 text-black active:bg-blue-500 
+            font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+              type="button"
+              onClick={() => setShowModal(true)}
+            >
+              Add New Habit
+            </button>
+            {showModal ? (
+              <>
+                <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                  <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                        <h3 className="text-3xl font=semibold">Create Habit</h3>
+                        <button
+                          className="bg-transparent border-0 text-black float-right"
+                          onClick={() => setShowModal(false)}
+                        >
+                          <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                            x
+                          </span>
+                        </button>
+                      </div>
+                      <div className="relative p-6 flex-auto">
+                        <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
+                          <label className="block text-black text-sm font-bold mb-1">
+                            Title
+                          </label>
+                          <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                          <label className="block text-black text-sm font-bold mb-1">
+                            Description
+                          </label>
+                          <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                          <label className="block text-black text-sm font-bold mb-1">
+                            Tags
+                          </label>
+                          <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                          <label className="block text-black text-sm font-bold mb-1">
+                            Repeat
+                          </label>
+                          <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                        </form>
+                      </div>
+                      <div className="flex items-center justify-center p-6 border-t border-solid border-blueGray-200 rounded-b">
+                        <button
+                          className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Save
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+          </>
+        );
+      };      
+        
 export default Habits;
