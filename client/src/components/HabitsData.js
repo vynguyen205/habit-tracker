34c3006717b9699e7fe8d22649f276e2b9f58269
dennismacc <dennis.maccarthy@gmail.com>
2022-08-20 10:47:43 -0700

    adding more styling elements to dashboard

--- client/src/components/HabitsData.js
+++ client/src/components/HabitsData.js
@@ -76,11 +76,8 @@ function Habits() {
             </ul>
         </div>
             <button
-              className="bg-blue-200 text-black active:bg-blue-500 
-            font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-              onClick={() => setShowModal(true)}
-            >
+              className="bg-blue-200 text-black active:bg-blue-500 font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+              type="button" onClick={() => setShowModal(true)}>
               Add New Habit
             </button>
             {showModal ? (
