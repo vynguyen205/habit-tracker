5729822a85e85d91ab021b1e7cebe7641dd75004
Vy Nguyen <vynguyen0520@gmail.com>
2022-08-20 12:44:41 -0700

    deleted some stuff pulled from marinas branch

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,92 +1,118 @@
 import React, { useState } from 'react';
+import { Form, Button, Alert } from 'tailwindcss';
 import { ADD_USER } from '../utils/Mutations';
 import { useMutation } from '@apollo/client';
 import { QUERY } from '../utils/Queries';
 
-import FlatButton from "material-ui/FlatButton";
-import RaisedButton from "material-ui/RaisedButton";
-import TextField from "material-ui/TextField";
-import PasswordStr from "./components/PasswordStr";
-
+import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
-const SignUpForm = ({
-  history,
-  onSubmit,
-  onChange,
-  errors,
-  user,
-  score,
-  btnTxt,
-  type,
-  pwMask,
-  onPwChange
-}) => {
+
+
+const SignupForm = () => {
+  const [ add_user ] = useMutation(ADD_USER);
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '', confirmPassword: '' }); 
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const newUser = add_user (userFormData.username, userFormData.email, userFormData.password);
+
+    if (newUser) {
+
+    }
+
+    }
+
+    try {
+      const response = createUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
 
   return (
-    <div className="loginBox">
-      <h1>Sign Up</h1>
-      {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+    <div>
+          <div>
+              <h1>
+                  Sign Up
+              </h1>
 
-      <form onSubmit={onSubmit}>
-        <TextField
-          name="username"
-          floatingLabelText="user name"
-          value={user.username}
-          onChange={onChange}
-          errorText={errors.username}
-        />
-        <TextField
-          name="email"
-          floatingLabelText="email"
-          value={user.email}
-          onChange={onChange}
-          errorText={errors.email}
-        />
-        <TextField
-          type={type}
-          name="password"
-          floatingLabelText="password"
-          value={user.password}
-          onChange={onPwChange}
-          errorText={errors.password}
-        />
+              <form onSubmit={handleFormSubmit}>
+                <div>
+                      <input
+                          type='username'
+                          id='username'
+                          placeholder='Username'
+                          name='username'
+                          value={userFormData.username}
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div>
+                      <input
+                          type='email'
+                          id='email'
+                          placeholder='Email'
+                          name='email'
+                          value={userFormData.email}
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div>
+                      <input
+                          type='password'
+                          id='password'
+                          placeholder='Password'
+                          name='password'
+                          value={userFormData.password}
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div>
+                      <input
+                          type='password'
+                          id='password'
+                          placeholder='Confirm Password'
+                          name='password'
+                          value={userFormData.password}
+                          onChange={handleInputChange}
+                      />
+                  </div>
 
-        <div className="pwStrRow">
-          {score >= 1 && (
-            <div>
-              <PasswordStr score={score} /> 
-              <FlatButton 
-                className="pwShowHideBtn" 
-                label={btnTxt} onClick={pwMask} 
-                style={{position: 'relative', left: '50%', transform: 'translateX(-50%)'}} 
-              />
-            </div>
-            )} 
-        </div>
-        <TextField
-          type={type}
-          name="pwconfirm"
-          floatingLabelText="confirm password"
-          value={user.pwconfirm}
-          onChange={onChange}
-          errorText={errors.pwconfirm}
-        />
-        <br />
-        <RaisedButton
-          className="signUpSubmit"
-          primary={true}
-          type="submit"
-          label="submit"
-        />
-      </form>
-      <p>
-        Aleady have an account? <br />
-        <a href="/">Log in here</a>
-      </p>
-    </div>
-  );
-};
+                  <div>
+                      <button>
+                          Sign Up
+                      </button>
+                  </div>
+              </form>
+          </div>
+      </div>
+);
 
-
-export default SignUpForm;
+export default SignupForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { useQuery } from '@apollo/client';
-// import { QUERY } from '../utils/Queries';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY } from '../utils/Queries';
     
-//import LoginForm from '../components/LoginForm';
-import SignUpContainer from '../components/SignUpContainer';
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import LoginForm from '../components/LoginForm';
+// import SignupForm from '../components/SignupForm';
 
-const Home = () => (
-    <MuiThemeProvider>
-        <SignUpContainer />
-    </MuiThemeProvider>
-);
 
+const Home = () => {
+    return (
+        <div className='h-[100vh] flex justify-center items-center'>
+            {/* <h1 className="text-indigo-900 p-4 h-12 shadow-lg font-semibold w-full text-center"> Groovy </h1> */}
+            <LoginForm />
+            {/* <SignupForm /> */}
+        </div>
+    )
+}
 
 export default Home;
