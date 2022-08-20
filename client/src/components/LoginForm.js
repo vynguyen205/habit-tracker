ad8eac558ed459f9858beac607de7aadef3cade6
Vy Nguyen <vynguyen0520@gmail.com>
2022-08-20 12:53:06 -0700

    deleted stuff

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,80 +1,54 @@
 // // see SignupForm.js for comments
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// // import Auth from '../utils/auth';
+// import Auth from '../utils/auth';
 
-// const LoginForm = () => {
+const LoginForm = () => {
 
-//   const [errorMessages, setErrorMessages] = useState({});
-//   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  
 //   setUserFormData({
 //     username: '',
 //     email: '',
 //     password: '',
 //   });
 
-//   const errors = {
-//     uname: "invalid username",
-//     pass: "invalid password"
-//   };
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
 
-//   const handleSubmit = (event) => {
-//     //Prevent page reload
-//     event.preventDefault();
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
 
-//     var { uname, pass } = document.forms[0];
+    var { uname, pass } = document.forms[0];
 
-//     // Find user login info
-//     const userData = setUserFormData.find((user) => user.username === uname.value);
+    // Find user login info
+    // const userData = setUserFormData.find((user) => user.username === uname.value);
 
-//     if (userData) {
-//       if (userData.password !== pass.value) {
-//         // Invalid password
-//         setErrorMessages({ name: "pass", message: errors.pass });
-//       } else {
-//         setIsSubmitted(true);
-//       }
-//     } else {
-//       // Username not found
-//       setErrorMessages({ name: "uname", message: errors.uname });
-//     }
-//   };
+    // if (userData) {
+    //   if (userData.password !== pass.value) {
+    //     // Invalid password
+    //     setErrorMessages({ name: "pass", message: errors.pass });
+    //   } else {
+    //     setIsSubmitted(true);
+    //   }
+    // } else {
+    //   // Username not found
+    //   setErrorMessages({ name: "uname", message: errors.uname });
+    // }
+    return (
+        <div className='bg-gradient-to-r p-4 shadow-lg rounded-2xl py-8 from-pink-500 to-violet-500 via-fuchsia-400'>
+              <div className="">
+                  <h1 >
+                      Login 🔐
+                  </h1>
+              </div>
+          </div>
+        );
+  };
+}; 
 
-
-//   const renderErrorMessage = (name) =>
-//   name === errorMessages.name && (
-//     <div className="error">{errorMessages.message}</div>
-//   );
-
-//   const renderForm = (
-//     <div className="form">
-//       <form onSubmit={handleSubmit}>
-//         <div className="input-container">
-//           <input type="text" name="uname" id="username" placeholder='username' required />
-//           {renderErrorMessage("uname")}
-//         </div>
-//         <div className="input-container">
-//           <input type="password" name="pass" id="password" placeholder="password" required />
-//           {renderErrorMessage("pass")}
-//         </div>
-//         <div className="button-container">
-//           <buttton> Login </buttton>
-//         </div>
-//       </form>
-//     </div>
-//   );
-
-//   return (
-//     <div className="app">
-//       <div className="login-form">
-//         <div className="title"></div>
-//         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-//       </div>
-//     </div>
-//   );
-
-//   };
-
-// export default LoginForm;
+export default LoginForm;
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,7 +8,6 @@ import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 
-
 const SignupForm = () => {
   const [ add_user ] = useMutation(ADD_USER);
   // set initial form state
