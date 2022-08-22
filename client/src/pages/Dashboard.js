1528a85df1700ae1e8657b95f630d1fe3741681b
dennismacc <dennis.maccarthy@gmail.com>
2022-08-21 22:12:58 -0700

    adding data to init dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,128 +1,83 @@
 // Import file dependencies
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import Menu from '../components/Menu';
+import HabitList from '../components/Habits/Habits';
+import TodoList from '../components/Todos/Todos';
+import AddHabit from '../components/Habits/addHabit';
+import AddTodo from '../components/Todos/addTodo';
 import { Link } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 
 // Display landing page
 function Dashboard() {
     const [showModal, setShowModal] = useState(false);
+    const [showAddHabit, setShowAddHabit] = useState(false);
+    const [habits, setHabits] = useState([]);
+    const [showAddTodo, setShowAddTodo] = useState(false);
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const getHabits = async () => {
+            const habitsFromServer = await fetchHabits();
+            setHabits(habitsFromServer);
+        }
+        getHabits();
+    }, []);
+
+    useEffect(() => {
+        const getTodos = async () => {
+            const todosFromServer = await fetchTodos()
+            setTodos(todosFromServer)
+        }
+
+        getTodos()
+    }, [])
+
+
+    // Fetch habits from server
+    const fetchHabits = async () => {
+        const habitRes = await fetch('/api/habits');
+        const habitData = await habitRes.json();
+
+        return habitData;
+    }
+
+    // Fetch Tasks
+    const fetchTodos = async () => {
+        const taskRes = await fetch('/api/tasks');
+        const taskData = await taskRes.json()
+
+        return taskData;
+    }
+
+
     return (
         <>
             <div className='flex-column bg-lightOrange h-[100vh] w-[100vw]'>
                 <Navbar />
-                <Menu />
-                    {/* Add new habit button */}
-                    <button
-                        className="bg-lightBlue text-white active:bg-darkBlue font-bold px-6 py-3 rounded-shadow hover:bg-darkBlue hover:shadow-lg outline-none focus:outline-none rounded-3xl mr-1 mb-1"
-                        type="button" onClick={() => setShowModal(true)}>
-                        Add New Habit
-                    </button>
-                    {showModal ? (
-                        <>
-                            <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                        <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                            <h3 className="text-3xl font=semibold">Create Habit</h3>
-                                            <button
-                                                className="bg-transparent border-0 text-black float-right"
-                                                onClick={() => setShowModal(false)}
-                                            >
-                                                <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
-                                                    X
-                                                </span>
-                                            </button>
-                                        </div>
-                                        <div className="relative p-6 flex-auto">
-                                            <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Title
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Description
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Tags
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Repeat
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                            </form>
-                                        </div>
-                                        <div className="flex items-center justify-center p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                            <button
-                                                className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                                                type="button"
-                                                onClick={() => setShowModal(false)}
-                                            >
-                                                Save
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </>
-                    ) : null}
-                    {/* Add new todo button */}
-                    <button
-                        className="bg-lightBlue text-white active:bg-darkBlue font-bold px-6 py-3 rounded-shadow hover:bg-darkBlue hover:shadow-lg outline-none focus:outline-none rounded-3xl mr-1 mb-1"
-                        type="button"
-                        onClick={() => setShowModal(true)}
-                    >
-                        Add New Daily
-                    </button>
-                    {showModal ? (
-                        <>
-                            <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                        <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                            <h3 className="text-3xl font=semibold">Create Todo</h3>
-                                            <button
-                                                className="bg-transparent border-0 text-black float-right"
-                                                onClick={() => setShowModal(false)}
-                                            >
-                                                <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
-                                                    X
-                                                </span>
-                                            </button>
-                                        </div>
-                                        <div className="relative p-6 flex-auto">
-                                            <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Title
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Description
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                                <label className="block text-black text-sm font-bold mb-1">
-                                                    Tags
-                                                </label>
-                                                <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                            </form>
-                                        </div>
-                                        <div className="flex items-center justify-center p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                            <button
-                                                className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                                                type="button"
-                                                onClick={() => setShowModal(false)}
-                                            >
-                                                Save
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </>
-                    ) : null}
+                <div>
+                    <Menu />
                 </div>
+                <div>
+                    <AddHabit onAdd={() => setShowAddHabit(!showAddHabit)} showAdd={showAddHabit} />
+                    <AddTodo onAdd={() => setShowAddTodo(!showAddTodo)} showAdd={showAddTodo} />
+                    
+                    {showAddHabit && <AddHabit onAdd={AddHabit} />}
+                    {habits.length > 0 ? (
+                        <HabitList habits={habits} />
+                    ) : (
+                            <h3>No habits yet</h3>
+                    )}
+
+                    {showAddTodo && <AddTodo onAdd={AddTodo} />}
+                    {todos.length > 0 ? (
+                        <TodoList todos={todos} />
+                    ) : (
+                            <h3>No todos yet</h3>
+                    )}
+                </div>
+            </div>
         </>
     );
 }
