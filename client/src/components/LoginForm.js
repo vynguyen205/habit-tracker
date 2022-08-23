32a47295097229e8a769189be567705d878a7550
dennismacc <dennis.maccarthy@gmail.com>
2022-08-22 23:10:51 -0700

    styling not working. problem solved "

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,20 +1,26 @@
-// // see SignupForm.js for comments
-import React, { useState } from 'react';
-
-import { loginUser } from '../utils/API';
-import AuthService from '../utils/Auth';
-
+import React, { useState } from "react";
+import { loginUser } from "../utils/API";
+import AuthService from "../utils/Auth";
 const LoginForm = () => {
-
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-//   setUserFormData({
-//     username: '',
-//     email: '',
-//     password: '',
-//   });
-
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    try {
+      const response = await loginUser(userFormData);
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
       const { token, user } = await response.json();
       console.log(user);
       AuthService.login(token);
@@ -22,63 +28,41 @@ const LoginForm = () => {
       console.error(err);
       setShowAlert(true);
     }
-
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     });
   };
-
   return (
       <div>
             <div>
                 <h1>
-                    Login 🔐
+                    Login :closed_lock_with_key:
                 </h1>
-
                 <form onSubmit={handleFormSubmit}>
                     <div>
-                        <input htmlFor='email'
-                            type='email'
-                            id='email'
-                            placeholder='Email'
+                        <input htmlFor="email"
+                            type="email"
+                            id="email"
+                            placeholder="Email"
                         />
                     </div>
                     <div>
-                        <input htmlFor='password'
-                            type='password'
-                            id='password'
-                            placeholder='Password'
+                        <input htmlFor="password"
+                            type="password"
+                            id="password"
+                            placeholder="Password"
                         />
                     </div>
-
-    const { uname, pass } = document.forms[0];
-
-    // Find user login info
-    // const userData = setUserFormData.find((user) => user.username === uname.value);
-
-    // if (userData) {
-    //   if (userData.password !== pass.value) {
-    //     // Invalid password
-    //     setErrorMessages({ name: "pass", message: errors.pass });
-    //   } else {
-    //     setIsSubmitted(true);
-    //   }
-    // } else {
-    //   // Username not found
-    //   setErrorMessages({ name: "uname", message: errors.uname });
-    // }
-    return (
-        <div className='bg-gradient-to-r p-4 shadow-lg rounded-2xl py-8 from-pink-500 to-violet-500 via-fuchsia-400'>
-              <div className="">
-                  <h1 >
-                      Login 🔐
-                  </h1>
-              </div>
-          </div>
-        );
-  };
-}; 
-
+                    <div>
+                        <button>
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+  );
+};
 export default LoginForm;
