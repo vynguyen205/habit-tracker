18fc01d08080b4287c36e7a68271677d3f9ede56
Marina H <mxhuisken@gmail.com>
2022-08-27 13:38:26 -0700

    signup page works

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
               element={<Home />}
             />
             <Route
-              path="/Dashboard/:userId"
+              path="/Dashboard/username"
               element={<Dashboard />}
             />
             <Route
