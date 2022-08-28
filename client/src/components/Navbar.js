1625769ee53b3612d935c4786fb3d1e29acd9cb6
dennismacc <dennis.maccarthy@gmail.com>
2022-08-28 03:45:35 -0700

    add animations 🤷

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,11 +5,12 @@ import "../App.css";
 
 function Navbar () {
     return (
-      <nav className='flex items-center justify-between flex-wrap bg-lightBlue p-6'>
+      <nav className='flex items-center justify-between flex-wrap bg-lightBlue p-4'>
         <Link to="/Dashboard/:userId" className='flex items-center flex-shrink-0 mr-6'>
-          <img src={require('../assets/bus.jpeg')} alt='logo' className='w-30 h-20' />
+          <img src={require('../assets/bus.jpeg')} alt='logo' className='w-32 h-22 animate-bounce' />
         </Link>
-        <button onClickCapture={logoutUser} className='inline-block text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-lightBlue hover:bg-white mt-4 md:mt-0'>Logout</button>
+        <h1 className='flex text-3xl text-white shadow-darkBlue font-bold font-mono animate-bounce'>🌼 Let's Groove 🌸</h1>
+        <button onClickCapture={logoutUser} className='inline-block animate-bounce text-lg px-5 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-lightBlue hover:bg-white mt-4 md:mt-0'>Logout</button>
       </nav>
     );
 }
